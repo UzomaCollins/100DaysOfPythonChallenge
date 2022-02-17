9135e7c2c17d58596f22f621895c5853b0f7c27c
Uzoma Collins A <uzomacollins30@gmail.com>
2022-02-18 05:59:03 +1300

    Add files via upload

---
+++ Day-1/Band_Name_Generator.py
@@ -0,0 +1,14 @@
+# Band Name Generator
+
+# Welcomes the user
+print("Welcome to the Band Name Generator.")
+
+# asks user for an input
+user_city = input("What is the city you grew up in?\n")
+
+# ask user for the name of their pet
+user_pet = input("What's the name of your pet?\n")
+
+# combine the name of their city and pet to
+# form their Band Name and print it out
+band_name = print("Your Band Name could be...", user_city + " " + user_pet)
---
+++ Day-1/practice.py
@@ -0,0 +1,54 @@
+# Day1 of 100DaysOfCode
+# Printing, Commenting, Debugging, String Manipulation and Variables
+
+
+# For debugging, pay close attention to your IDE and written
+# program or can use thonny IDE for debugging
+# To print using the print function
+
+print('Hello!, Uzoma Collins Arinze')
+
+#Work Solution
+print("Day-1 - Python Print Function ")
+print("The Function is declared like this:")
+print('print("what to print")')
+print()
+
+#Printing and lines
+print("Hello, world!")
+print("Hello, world!")
+print("Hello, world!")
+print()
+
+#Best way to print in a single line
+print("Hello, world!\nHello, world!\nHello, world! ")
+print()
+
+#String concatination
+print("Hello" + " " + "Collins")
+# print("Hello " + "Collins")
+# print("Hello " + "Collins")
+
+
+# using the input function
+name = input("what's your name?\n")
+print("Your name is:", name)
+
+
+# Another method
+print("Hello " + input("What's your name?"))
+
+# input exercise: print the number of characters in a word
+name = str(input("What's your name?\n"))
+print("Your name is",name,"and it has", len(name),"characters")
+
+
+# Variables: They are simply used to store data for processing
+# Name your variable according to your problem space
+# Ensure you follow the rules according to the documentation
+
+a = input("input a ")
+b = input("input b ")
+
+print("The value of 'a' is:", b)
+print("The value of 'b' is:", a)
