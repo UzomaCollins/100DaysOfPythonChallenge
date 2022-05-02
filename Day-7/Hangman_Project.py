ded0e3162c4adad17265c4fb62b2f36275c5d32a
Uzoma Collins A <uzomacollins30@gmail.com>
2022-05-02 20:52:00 +1300

    Update Hangman_Project.py

--- Day-7/Hangman_Project.py
+++ Day-7/Hangman_Project.py
@@ -1,19 +1,5 @@
+import random
 
-
-# HANGMAN GAME
-
-print('''
- _   _                                         
-| | | |                                        
-| |_| | __ _ _ __   __ _ _ __ ___   __ _ _ __  
-|  _  |/ _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
-| | | | (_| | | | | (_| | | | | | | (_| | | | |
-\_| |_/\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
-                    __/ |                      
-                   |___/       
-''')
-
-# ASCII for Hangman
 stages = ['''
   +---+
   |   |
@@ -22,8 +8,6 @@ stages = ['''
  / \  |
       |
 =========
-
-
 ''', '''
   +---+
   |   |
@@ -32,7 +16,6 @@ stages = ['''
  /    |
       |
 =========
-
 ''', '''
   +---+
   |   |
@@ -41,7 +24,6 @@ stages = ['''
       |
       |
 =========
-
 ''', '''
   +---+
   |   |
@@ -49,9 +31,7 @@ stages = ['''
  /|   |
       |
       |
-=========
-
-''', '''
+=========''', '''
   +---+
   |   |
   O   |
@@ -59,7 +39,6 @@ stages = ['''
       |
       |
 =========
-
 ''', '''
   +---+
   |   |
@@ -68,7 +47,6 @@ stages = ['''
       |
       |
 =========
-
 ''', '''
   +---+
   |   |
@@ -76,62 +54,266 @@ stages = ['''
       |
       |
       |
-========='''
+=========
+''']
 
-		  ]
+logo = ''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    '''
 
+word_list = [
+	'abruptly',
+	'absurd',
+	'abyss',
+	'affix',
+	'askew',
+	'avenue',
+	'awkward',
+	'axiom',
+	'azure',
+	'bagpipes',
+	'bandwagon',
+	'banjo',
+	'bayou',
+	'beekeeper',
+	'bikini',
+	'blitz',
+	'blizzard',
+	'boggle',
+	'bookworm',
+	'boxcar',
+	'boxful',
+	'buckaroo',
+	'buffalo',
+	'buffoon',
+	'buxom',
+	'buzzard',
+	'buzzing',
+	'buzzwords',
+	'caliph',
+	'cobweb',
+	'cockiness',
+	'croquet',
+	'crypt',
+	'curacao',
+	'cycle',
+	'daiquiri',
+	'dirndl',
+	'disavow',
+	'dizzying',
+	'duplex',
+	'dwarves',
+	'embezzle',
+	'equip',
+	'espionage',
+	'euouae',
+	'exodus',
+	'faking',
+	'fishhook',
+	'fixable',
+	'fjord',
+	'flapjack',
+	'flopping',
+	'fluffiness',
+	'flyby',
+	'foxglove',
+	'frazzled',
+	'frizzled',
+	'fuchsia',
+	'funny',
+	'gabby',
+	'galaxy',
+	'galvanize',
+	'gazebo',
+	'giaour',
+	'gizmo',
+	'glowworm',
+	'glyph',
+	'gnarly',
+	'gnostic',
+	'gossip',
+	'grogginess',
+	'haiku',
+	'haphazard',
+	'hyphen',
+	'iatrogenic',
+	'icebox',
+	'injury',
+	'ivory',
+	'ivy',
+	'jackpot',
+	'jaundice',
+	'jawbreaker',
+	'jaywalk',
+	'jazziest',
+	'jazzy',
+	'jelly',
+	'jigsaw',
+	'jinx',
+	'jiujitsu',
+	'jockey',
+	'jogging',
+	'joking',
+	'jovial',
+	'joyful',
+	'juicy',
+	'jukebox',
+	'jumbo',
+	'kayak',
+	'kazoo',
+	'keyhole',
+	'khaki',
+	'kilobyte',
+	'kiosk',
+	'kitsch',
+	'kiwifruit',
+	'klutz',
+	'knapsack',
+	'larynx',
+	'lengths',
+	'lucky',
+	'luxury',
+	'lymph',
+	'marquis',
+	'matrix',
+	'megahertz',
+	'microwave',
+	'mnemonic',
+	'mystify',
+	'naphtha',
+	'nightclub',
+	'nowadays',
+	'numbskull',
+	'nymph',
+	'onyx',
+	'ovary',
+	'oxidize',
+	'oxygen',
+	'pajama',
+	'peekaboo',
+	'phlegm',
+	'pixel',
+	'pizazz',
+	'pneumonia',
+	'polka',
+	'pshaw',
+	'psyche',
+	'puppy',
+	'puzzling',
+	'quartz',
+	'queue',
+	'quips',
+	'quixotic',
+	'quiz',
+	'quizzes',
+	'quorum',
+	'razzmatazz',
+	'rhubarb',
+	'rhythm',
+	'rickshaw',
+	'schnapps',
+	'scratch',
+	'shiv',
+	'snazzy',
+	'sphinx',
+	'spritz',
+	'squawk',
+	'staff',
+	'strength',
+	'strengths',
+	'stretch',
+	'stronghold',
+	'stymied',
+	'subway',
+	'swivel',
+	'syndrome',
+	'thriftless',
+	'thumbscrew',
+	'topaz',
+	'transcript',
+	'transgress',
+	'transplant',
+	'triphthong',
+	'twelfth',
+	'twelfths',
+	'unknown',
+	'unworthy',
+	'unzip',
+	'uptown',
+	'vaporize',
+	'vixen',
+	'vodka',
+	'voodoo',
+	'vortex',
+	'voyeurism',
+	'walkway',
+	'waltz',
+	'wave',
+	'wavy',
+	'waxy',
+	'wellspring',
+	'wheezy',
+	'whiskey',
+	'whizzing',
+	'whomever',
+	'wimpy',
+	'witchcraft',
+	'wizard',
+	'woozy',
+	'wristwatch',
+	'wyvern',
+	'xylophone',
+	'yachtsman',
+	'yippee',
+	'yoked',
+	'youthful',
+	'yummy',
+	'zephyr',
+	'zigzag',
+	'zigzagging',
+	'zilch',
+	'zipper',
+	'zodiac',
+	'zombie',
+]
 
-
-import random
-
-# create a lis of words
-word_list = ('ant baboon badger bat bear beaver camel cat clam cobra cougar '
-             'coyote crow deer dog donkey duck eagle ferret fox frog goat '
-             'goose hawk lion lizard llama mole monkey moose mouse mule newt '
-             'otter owl panda parrot pigeon python rabbit ram rat raven '
-             'rhino salmon seal shark sheep skunk sloth snake spider '
-             'stork swan tiger toad trout turkey turtle weasel whale wolf '
-             'wombat zebra ').split()
-
-
-# randomly chose word
+print(logo)
+end_of_game = False
 chosen_word = random.choice(word_list)
+lives = len(stages) - 1
 
-# Number of player live's.
-lives = 6
-
-# Number of stages
-print(stages[-1])
-
-#create an empty list
 display = []
-
-# add symbol into the list
 for letter in chosen_word:
-    display.append("_")
-
-# make a print
+	display += "_"
 print(display)
 
-# use a while loop to make conditions on lives
-while lives > 0:  # Check for number of lives
-    guess = input("Choose a letter: ").lower()
-    if guess not in chosen_word:  # reduce lives in guess not on chosen_word
-        lives -= 1
-        print(stages[lives])
-    if lives == 0:
-        print("You have no more lives try again")
+while not end_of_game:
+	guess = input("Guess a letter: ").lower()
+	for position in range(0, len(chosen_word)):
+		letter = chosen_word[position]
+		if letter == guess:
+			display[position] = letter
 
-    # If index of letter in word matches the index of display replace display index with letter
-    for index, elem in enumerate(chosen_word):
-        if elem == guess:
-            display[index] = guess
-    print(display)
+	if guess not in chosen_word:
+		lives -= 1
+		if lives == 0:
+			end_of_game = True
+			print("You lose.")
 
-    # When "_" is no longer present in display than you win.
-    if "_" not in display:
-        lives = 0
-        print(" Yay you win!")
-        break
-# make the final print
-print("The answer was: ",chosen_word)
+			# make the final print
+			print("The answer was: ", chosen_word)
+
+	print(f"{' '.join(display)}")
+	print(display)
+
+	if "_" not in display:
+		end_of_game = True
+		print("You Win!")
+	print(stages[lives])
+	
